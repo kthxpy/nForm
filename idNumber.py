4ac3e51e5892bf3a2c845adf2bbc963667ba6ca0
kthxpy <ferrerorochermfg@gmail.com>
2017-11-27 10:53:25 +0100

    functions covered

---
+++ idNumber.py
@@ -0,0 +1,50 @@
+import re
+
+def isFormatValid(number):
+    if len(number) != 11:
+        return False
+    if number[6] != "/":
+        return False
+
+    return True
+
+def isDivisibleby11(number):
+    number = list(number)
+    number.pop(6)
+    number = "".join(number)
+    number = int(number)
+    mod = number % 11
+
+    if mod == 0:
+        return True
+    else:
+        return False
+
+def maleOrFemale(number):
+    if int(number[2:4]) > 12:
+        return "žena"
+    else:
+        return "muž"
+
+def birthDate(number):
+    year = 0
+    month = 0
+    day = 0
+    if maleOrFemale(number) == "žena":
+        mod1 = int(number[2:4])% 70
+        print(mod1)
+        if mod1 == 0:
+            month = int(number[2:4]) - 70
+        else:
+            month = int(number[2:4]) - 50
+    else:
+        month = int(number[2:4])
+
+    day = int(number[4:6] )
+    year = int(number[0:2])
+
+    return(day, month, year )
+
+i = birthDate("736028/5163")
+
+print(i)
